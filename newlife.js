1b721dd058cd29ce660d48de1de8844b65fe6dbb
Kaiyu Zheng <kaiyutony@gmail.com>
2015-01-15 00:43:33 -0800

    Keep working. Slightly complicated

--- newlife.js
+++ newlife.js
@@ -13,8 +13,8 @@ function Conways(initArray, parentID, id, size) {
     this.rowBound = initArray.length; // where the visible part's index is
     this.colBound = initArray[0].length;
 
-    this.current = Conways.init2DArray(this.row, this.col);
-    this.successor = Conways.init2DArray(this.row, this.col);
+    this.current = this.init2DField(this.row, this.col, initArray);
+    this.successor = this.init2DField(this.row, this.col, null);
 
     this.active = {}; // used for improving performance -- only care about active cells
 
@@ -48,16 +48,17 @@ function Conways(initArray, parentID, id, size) {
 // to the 'current' array, otherwise, will apply the 
 // new life status to successor array
 
-
+var gens = 0;
 Conways.prototype.reproduce = function() {
     for (id in this.active) {
-        if (this.active.hasOwnProperty(id)) {
-            var rc = id.split('-');
-            var status = this.destiny(rc[0], rc[1]);
-        }
+        var rc = id.split('-');
+        var status = this.destiny(parseInt(rc[0]), parseInt(rc[1]));
     }
     this.current = this.successor.slice();
-    this.successor = Conways.init2DArray(this.row, this.col);
+    this.successor = this.init2DArray(this.row, this.col, null);
+    if (gens >= 2) {
+        this.deleteOldActive(gens - 2); // clean out the old actives
+    }
 /*    for (var i = 0; i < this.row; i++) {
         for (var j = 0; j < this.col; j++) {
             var status = this.destiny(i, j); // could be used for track
@@ -99,9 +100,10 @@ Conways.prototype.destiny = function(r, c) {
             count = this.current[r-1][c-1] + this.current[r-1][c] + this.current[r][c-1]
                 + this.current[r+1][c-1] + this.current[r+1][c];
         } else {
-            count = this.current[r-1][c-1] + this.current[r-1][c] + this.current[r-1][c+1]
-                + this.current[r][c-1] + this.current[r][c+1] + this.current[r+1][c-1]
-                + this.current[r+1][c] + this.current[r+1][c+1];
+            count = this.current[r-1][c-1] + this.current[r-1][c]
+                + this.current[r-1][c+1] + this.current[r][c-1]
+                + this.current[r+1][c] + this.current[r+1][c+1]
+                + this.current[r][c+1] + this.current[r+1][c-1];
         }
     }
  
@@ -116,6 +118,11 @@ Conways.prototype.destiny = function(r, c) {
     
     if (this.current[r][c] == 0 && count == 3) {
         result = 1;
+    }
+
+    // change
+    if (this.current[r][c] != result) {
+        this.addActive(r, c);
     }
 
     if (result == 1) {
@@ -133,7 +140,12 @@ Conways.prototype.draw = function() {
         var htmlString = "<div id='" + this.id + "' class='conways-board'>";
         for (var i = 0; i < this.row; i++) {
             for (var j = 0; j < this.col; j++) {
-                htmlString += this.create(i, j, this.current[i][j]);
+                if (this.inViewPort(i, j)) {
+//                    alert('r; ' + i + ' ; c: ' + j);
+                    htmlString += this.create(i, j, this.current[i][j], true);
+                } else {
+                    htmlString += this.create(i, j, this.current[i][j], false);
+                }
             }
         }
         $('#' + this.parentID).append(htmlString);
@@ -142,8 +154,8 @@ Conways.prototype.draw = function() {
             'height' : this.size - 2 + "px"
         });
         $('#' + this.id).css({
-            'width' : this.size * this.col + "px",
-            'height' : this.size * this.row + "px"
+            'width' : this.size * this.colBound + "px",
+            'height' : this.size * this.rowBound + "px"
         });
     } else {
         for (var i = 0; i < this.row; i++) {
@@ -160,11 +172,14 @@ Conways.prototype.draw = function() {
 }
 
 // Creates one grid; Returns the html string
-Conways.prototype.create = function(r, c, alive) {
+Conways.prototype.create = function(r, c, alive, visible) {
     var id = r + '-' + c;
     var htmlString = "<div id='" + id + "' class='grid";
     if (alive) {
         htmlString += " alive";
+    }
+    if (!visible) {
+        htmlString += " hidden";
     }
     htmlString += "' ></div>";
     return htmlString;
@@ -172,8 +187,8 @@ Conways.prototype.create = function(r, c, alive) {
 
 // called when a grid is clicked
 Conways.prototype.clicked = function(id) {
-    var r = id.split('-')[0];
-    var c = id.split('-')[1];
+    var r = parseInt(id.split('-')[0]);
+    var c = parseInt(id.split('-')[1]);
     if (this.current[r][c] == 0) {
         this.current[r][c] = 1;
         $('#' + id).addClass("alive");
@@ -181,6 +196,7 @@ Conways.prototype.clicked = function(id) {
         this.current[r][c] = 0;
         $('#' + id).removeClass("alive");
     }
+    this.addActive(r, c); // changed because of clicking
 }
 
 // run this conway's game of life
@@ -208,75 +224,101 @@ Conways.prototype.clear = function() {
     }
     this.current = init2DArray(this.row, this.col);
     this.successor = init2DArray(this.row, this.col);
+    this.active = {};
     this.draw();
 }
 
 // adds the possible active cell indices into the 'active' object
+// gens is a global variable
 Conways.prototype.addActive = function(r, c) {
-    this.active[r + '-' + c] = 0;
+    this.active[r + '-' + c] = gens;
     if (r == 0) {
         if (c == 0) {
-            this.active[(r+1) + '-' + c] = 0;
-            this.active[r + '-' + (c+1)]; 
-            this.active[(r+1)(c+1)];
+            this.active[(r+1) + '-' + c] = gens;
+            this.active[r + '-' + (c+1)] = gens; 
+            this.active[(r+1) + '-' + (c+1)] = gens;
         } else if (c == this.col - 1) {
-            this.active[r + '-' + (c-1)];
-            this.active[(r+1) + '-' + c];
-            this.active[(r+1) + '-' + (c-1)];
+            this.active[r + '-' + (c-1)] = gens;
+            this.active[(r+1) + '-' + c] = gens;
+            this.active[(r+1) + '-' + (c-1)] = gens;
         } else {
-            this.active[r + '-' + (c-1)]; 
-            this.active[r + '-' + (c+1)]; 
-            this.active[(r+1) + '-' + (c-1)];
-            this.active[(r+1)+ '-' + c];
-            this.active[(r+1) + '-' + (c+1)];
+            this.active[r + '-' + (c-1)] = gens; 
+            this.active[r + '-' + (c+1)] = gens; 
+            this.active[(r+1) + '-' + (c-1)] = gens;
+            this.active[(r+1)+ '-' + c] = gens;
+            this.active[(r+1) + '-' + (c+1)] = gens;
         }
     } else if (r == this.row - 1) {
         if (c == 0) {
-            this.active[(r-1) + '-' + c]; 
-            this.active[r + '-' + (c+1)];
-            this.active[(r-1) + '-' + (c+1)];
+            this.active[(r-1) + '-' + c] = gens; 
+            this.active[r + '-' + (c+1)] = gens;
+            this.active[(r-1) + '-' + (c+1)] = gens;
         } else if (c == this.col - 1) {
-            this.active[r + '-' + (c-1)];
-            this.active[(r-1) + '-' + c];
-            this.active(r-1](c-1);
+            this.active[r + '-' + (c-1)] = gens;
+            this.active[(r-1) + '-' + c] = gens;
+            this.active[(r-1) + '-' + (c-1)] = gens;
         } else {
-            this.active[r + '-' + (c-1)];
-            this.active[r + '-' + (c+1)];
-            this.active[(r-1) + '-' + (c-1)]; 
-            this.active[(r-1) + '-' + c];
-            this.active[(r-1) + '-' + (c+1);
+            this.active[r + '-' + (c-1)] = gens;
+            this.active[r + '-' + (c+1)] = gens;
+            this.active[(r-1) + '-' + (c-1)] = gens; 
+            this.active[(r-1) + '-' + c] = gens;
+            this.active[(r-1) + '-' + (c+1)] = gens;
         }
     } else {
         if (c == 0) {
-            this.active[(r-1) + '-' + c];
-            this.active[(r-1) + '-' + (c+1)];
-            this.active[r + '-' + (c+1)];
-            this.active[(r+1) + '-' + (c+1)];
-            this.active[(r+1)+ '-' + c];
+            this.active[(r-1) + '-' + c] = gens;
+            this.active[(r-1) + '-' + (c+1)] = gens;
+            this.active[r + '-' + (c+1)] = gens;
+            this.active[(r+1) + '-' + (c+1)] = gens;
+            this.active[(r+1)+ '-' + c] = gens;
         } else if (c == this.col - 1) {
-            this.active[(r-1) + '-' + (c-1)];
-            this.active[(r-1) + '-' + c];
-            this.active[r + '-' + (c-1)];
-            this.active[(r+1) + '-' + (c-1)]; 
-            this.active[(r+1)+ '-' + c];
+            this.active[(r-1) + '-' + (c-1)] = gens;
+            this.active[(r-1) + '-' + c] = gens;
+            this.active[r + '-' + (c-1)] = gens;
+            this.active[(r+1) + '-' + (c-1)] = gens; 
+            this.active[(r+1)+ '-' + c] = gens;
         } else {
-            this.active[(r-1) + '-' + (c-1)];
-            this.active[(r-1) + '-' + c];
-            this.active[(r-1) + '-' + (c+1)];
-            this.active[r + '-' + (c-1)];
-            this.active[r + '-' + (c+1)];
-            this.active[(r+1) + '-' + (c-1)];
-            this.active[(r+1)+ '-' + c]; 
-            this.active[(r+1) + '-' + (c+1);
+            this.active[(r-1) + '-' + (c-1)] = gens;
+            this.active[(r-1) + '-' + c] = gens;
+            this.active[(r-1) + '-' + (c+1)] = gens;
+            this.active[r + '-' + (c-1)] = gens;
+            this.active[r + '-' + (c+1)] = gens;
+            this.active[(r+1) + '-' + (c-1)] = gens;
+            this.active[(r+1)+ '-' + c] = gens; 
+            this.active[(r+1) + '-' + (c+1)] = gens;
         }
     }    
 }
 
-Conways.init2DArray = function(r, c) {
+Conways.prototype.inViewPort = function(r, c) {
+    return (r >= this.rowBound && r < this.rowBound + (this.row / 3))
+        && (c >= this.colBound && c < this.colBound + (this.col / 3));
+}
+
+// delete the older active cells with generation gens
+Conways.prototype.deleteOldActive = function(gens) {
+    for (id in this.active) {
+        if (this.active[id] == gens) {
+            delete this.active[id];
+        }
+    }
+}
+
+Conways.prototype.init2DField = function(r, c, initArray) {
     var arr = new Array(r);
+    var m = 0;
+    var n = 0;
     for (var i = 0; i < r; i++) {
         arr[i] = new Array(c);
         for (var j = 0; j < c; j++) {
+            if (initArray != null && this.inViewPort(i, j)) {
+                arr[i][j] = initArray[n][m];
+                m++;
+                if (m >= initArray[0].length) {
+                    m = 0;
+                    n++;
+                }
+            }
             arr[i][j] = 0;
         }
     }
